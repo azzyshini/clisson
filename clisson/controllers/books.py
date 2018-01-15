ef74dbd505b6c248cd05f8db14b24bf4189db198
Jane <shinigamideico@gmail.com>
2018-01-14 20:00:40 -0500

    new lines

--- clisson/controllers/books.py
+++ clisson/controllers/books.py
@@ -6,9 +6,12 @@ mod_books = Blueprint('books', __name__, url_prefix='/api/v1.0')
 @mod_books.route('/books.json', methods=['GET'])
 def books():
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM books''')
+    cur.execute('''SELECT id, title, author_name FROM books''')
     rv = cur.fetchall()
-    return str(rv)
+    boooks = []
+    for row in rv:
+        boooks.append({'id' row['id'], 'title': row['title'], 'author_name': row['author_name']})
+    return jsonify(boooks)
 
 @mod_books.route('/books/<int:book_id>.json', methods=['GET'])
 def book(book_id):
