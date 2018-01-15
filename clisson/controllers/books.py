de8f7376d90e0cbe4ec855d4386ccc1307d990bc
Jane <shinigamideico@gmail.com>
2018-01-14 20:31:10 -0500

    Hope this works

--- clisson/controllers/books.py
+++ clisson/controllers/books.py
@@ -16,6 +16,6 @@ def books():
 @mod_books.route('/books/<int:book_id>.json', methods=['GET'])
 def book(book_id):
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT id, title, author_name FROM books where id = book_id''')
-    rone = cur.fetchone()
-    return jsonify(rone)
+    cur.execute('''SELECT id, title, author_name FROM books where id = %d''', (book_id,))
+    row = cur.fetchone()
+    return jsonify(row)
