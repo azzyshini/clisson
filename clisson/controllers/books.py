dc1ccff90972d548e5f8c85fc2db7d07b9092e25
Jane <shinigamideico@gmail.com>
2018-01-14 20:01:47 -0500

    fixed

--- clisson/controllers/books.py
+++ clisson/controllers/books.py
@@ -10,7 +10,7 @@ def books():
     rv = cur.fetchall()
     boooks = []
     for row in rv:
-        boooks.append({'id' row['id'], 'title': row['title'], 'author_name': row['author_name']})
+        boooks.append({'id' : row['id'], 'title': row['title'], 'author_name': row['author_name']})
     return jsonify(boooks)
 
 @mod_books.route('/books/<int:book_id>.json', methods=['GET'])
