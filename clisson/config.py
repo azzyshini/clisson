bd6b8b2222737a318252be3eca76e30194de90b5
Jane <shinigamideico@gmail.com>
2018-01-14 19:48:14 -0500

    syntax

--- clisson/config.py
+++ clisson/config.py
@@ -7,12 +7,13 @@ class Config(object):
 
     BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-    MYSQL_DATABASE_HOST = 'localhost'
-    MYSQL_DATABASE_PORT = 3306
-    MYSQL_DATABASE_USER = 'clisson'
-    MYSQL_DATABASE_PASSWORD = 'clisson2017'
-    MYSQL_DATABASE_DB = 'clisson_library'
-    MYSQL_DATABASE_CHARSET = 'utf8mb4'
+    MYSQL_HOST = 'localhost'
+    MYSQL_PORT = 3306
+    MYSQL_USER = 'clisson'
+    MYSQL_PASSWORD = 'clisson2017'
+    MYSQL_DB = 'clisson_library'
+    MYSQL_CHARSET = 'utf8mb4'
+    MYSQL_USE_UNICODE = True
 
 class DevConfig(Config):
     DEBUG = True
