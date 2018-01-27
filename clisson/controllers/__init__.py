a359658b57b38cff7ceb10d14c2ac3b4c8dc51c3
Jane <shinigamideico@gmail.com>
2018-01-27 18:07:42 -0500

    got auth

--- clisson/controllers/__init__.py
+++ clisson/controllers/__init__.py
@@ -2,4 +2,4 @@ from clisson.controllers.books import mod_books
 from clisson.controllers.users import mod_users
 from clisson.controllers.checkouts import mod_checkouts
 from clisson.controllers.holds import mod_holds 
-from clisson.controllers.auth import mod_auth
+from clisson.controllers.authentication import mod_auth
