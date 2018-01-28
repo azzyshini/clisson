6515fae9df3805cd4fbf972cfe5f178d2404d9c9
Jane <shinigamideico@gmail.com>
2018-01-27 19:30:03 -0500

    row

--- clisson/controllers/users.py
+++ clisson/controllers/users.py
@@ -6,13 +6,13 @@ mod_users = Blueprint('users', __name__, url_prefix='/api/v1.0')
 @mod_users.route('/user/<string:username>.json', methods=['GET'])
 def user_by_name(username): 
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT id, email, first_name, last_name FROM users Where username = %s''', (username,))
+    cur.execute('''SELECT id, email, first_name, last_name FROM users WHERE username = %s''', (username,))
     rv = cur.fetchone()
     return jsonify({'id': row[0], 'email': row[1], 'first_name': row[2], 'last_name': row[3]})
 
 @mod_users.route('/user/<int:user_id>.json', methods=['GET'])
 def user_by_id(user_id): 
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT id, email, first_name, last_name FROM users Where id = %d''', (int(user_id),))
+    cur.execute('''SELECT id, email, first_name, last_name FROM users WHERE id = %s''', (user_id,))
     rv = cur.fetchone()
     return jsonify({'id': row[0], 'email': row[1], 'first_name': row[2], 'last_name': row[3]})
