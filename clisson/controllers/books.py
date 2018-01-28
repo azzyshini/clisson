3d9333b74c143b5dd05185866c8857e4e8115e64
Jane <shinigamideico@gmail.com>
2018-01-27 21:12:52 -0500

    image

--- clisson/controllers/books.py
+++ clisson/controllers/books.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request
+import io
+from flask import Blueprint, jsonify, request, send_file
 from clisson import app, mysql
 from clisson.controllers.common import requires_auth
 
@@ -25,3 +26,15 @@ def book(book_id):
         message = "Book with id {} not found".format(book_id)
         return jsonify({'message': message, 'status': 404}), 404
     return jsonify({'title': row[0], 'author_name': row[1], 'availability': row[2]})
+
+@mod_books.route('/books/cover/<int:book_id>', methods=['GET'])
+@requires_auth
+def book_cover(book_id):
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT book_cover FROM books WHERE id = %s''', (book_id,))
+    row = cur.fetchone()
+    if not row:
+        message = "Book cover with id {} not found".format(book_id)
+        return message, 404
+    cover = row[0]
+    return send_file(io.BytesIO(cover.read()), attachment_filename='book_cover_{}.png'.format(book_id), mimetype='image/png')
