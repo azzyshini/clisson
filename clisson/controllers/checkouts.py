141095043f6761e6a9bb1669b03e5ef64a58bbc7
Jane <shinigamideico@gmail.com>
2018-02-03 16:56:51 -0500

    inserts

--- clisson/controllers/checkouts.py
+++ clisson/controllers/checkouts.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from clisson import app, mysql
+from clisson.controllers.common import requires_auth
 
 mod_checkouts = Blueprint('checkouts', __name__, url_prefix='/api/v1.0')
 
@@ -10,5 +11,19 @@ def checkouts(id):
     rv = cur.fetchall()
     checkouts = []
     for row in rv:
-        checkouts.append({'title' : row[0], 'author': row[1], 'due_date': row[2]})
+        checkouts.append({'title': row[0], 'author': row[1], 'due_date': row[2]})
     return jsonify(checkouts)
+
+@mod_checkouts.route('/checkout.json', methods=['POST'])
+@requires_auth
+def checkout(): 
+	info = request.get_json(force=True, silent=True)
+	error = none
+	if info:
+		user_id = info.get("user_id")
+		book_id = info.get("book_id")
+    	cur = mysql.connection.cursor()
+    	cur.execute('''INSERT INTO checkouts (user_id, book_id, due_date) VALUES (%s, %s, NOW()+INTERVAL 2 week)''', (user_id, book_id,))
+    	cur.execute('''SELECT first_name, last_name, title, author, due_date FROM chekcouts JOIN users ON users.id = checkouts.user_id JOIN books ON books.id = checkouts.book_id WHERE user_id = %s AND book_id = %s''', (user_id, book_id,))
+    	rv = cur.fetchone()
+    	return jsonify({'first_name': row[0], 'last_name': row[1], 'title': row[2], 'author': row[3], 'due_date': row[4]})
--- clisson/controllers/holds.py
+++ clisson/controllers/holds.py
@@ -12,3 +12,16 @@ def holds(id):
     for row in rv:
         holds.append({'title' : row[0], 'author': row[1]})
     return jsonify(holds)
+
+@mod_holds.route('/hold.json', methods=['POST'])
+@requires_auth
+def hold(): 
+	info = request.get_json(force=True, silent=True)
+	if info:
+		user_id = info.get("user_id")
+		book_id = info.get("book_id")
+    	cur = mysql.connection.cursor()
+    	cur.execute('''INSERT INTO checkouts (user_id, book_id) VALUES (%s, %s)''', (user_id, book_id,))
+    	cur.execute('''SELECT first_name, last_name, title, author FROM holds JOIN users ON users.id = holds.user_id JOIN books ON books.id = holds.book_id WHERE user_id = %s AND book_id = %s''', (user_id, book_id,))
+    	rv = cur.fetchone()
+    	return jsonify({'first_name': row[0], 'last_name': row[1], 'title': row[2], 'author': row[3]})
