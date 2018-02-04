f35be0aefe40facdc227f1d9b54537e14b0aed28
Jane <shinigamideico@gmail.com>
2018-02-03 23:06:46 -0500

    new data

--- database/refresh-db.sh
+++ database/refresh-db.sh
@@ -3,3 +3,4 @@
 mysql -u clisson -p -e 'drop database if exists clisson_library;' 
 cat clisson_library.sql | mysql -u clisson -p
 cat insert.sql | mysql -u clisson -p clisson_library
+cat insert_book_covers.sql | mysql -u clisson -p clisson_library
