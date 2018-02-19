1ca56af081908da2f2ea1f7f7fbcd8d9ca10ad37
azzyshini <shinigamideico@gmail.com>
2018-02-19 11:46:37 -0500

    attempting

--- clisson/controllers/users.py
+++ clisson/controllers/users.py
@@ -15,11 +15,11 @@ def user_by_name(username):
         return jsonify({'message': message, 'status': 404}), 404
     return jsonify({'id': row[0], 'email': row[1], 'first_name': row[2], 'last_name': row[3], 'user_type_id': row[4]})
 
-@mod_users.route('/users/<string:user_id>.json', methods=['GET'])
+@mod_users.route('/users/<int:user_id>.json', methods=['GET'])
 @requires_auth
 def user_by_id(user_id): 
     cur = mysql.connection.cursor()
-    cur.execute('''SELECT id, email, first_name, last_name FROM users WHERE id = %s''', (user_id,))
+    cur.execute('''SELECT id, email, first_name, last_name FROM users WHERE id = %d''', (user_id,))
     row = cur.fetchone()
     if not row:
         message = "User with id {} not found".format(user_id)
